62b82381c63ac6bb1ae0d6a7bf9c0ef337c8e29e
Matt Williams <m@technovangelist.com>
2022-12-23 14:56:50 -0800

    modified mod.ts in .
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- mod.ts
+++ mod.ts
@@ -1,37 +1,39 @@
-import { addUserGrant, addGroupGrant, searchAllUsers, searchRole, searchUser, searchGroup, validateArgs, validateAccess } from "./infra.ts";
+import {
+	addUserGrant,
+	addGroupGrant,
+	searchAllUsers,
+	searchRole,
+	searchUser,
+	searchGroup,
+	validateAccess,
+} from "./infra.ts";
 import { Command } from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
 
-
 new Command()
-  .name("kubectl-infra")
-  .arguments("[user:string] [role:string]")
-  .action(async ()=>{
-    let output = "";
-    if (await validateAccess()) {
-    const { user, group, role} = await validateArgs(Deno.args);
-    if (Deno.args.length === 2) {
-      if (user.length>0) {
-      output = await addUserGrant(user, role);
-      } else if (group.length>0 ) {
-        output = await addGroupGrant(group, role);
-      }
-    } else if (Deno.args.length === 1) {
-      if (user.length > 0) {
-        output = await searchUser(user);
-      } else if (role.length > 0) {
-        output = searchRole(role);
-      } else if (group.length > 0) {
-        output = await searchGroup(group);
-      }
-    } else if (Deno.args.length === 0) {
-      searchAllUsers();
-    } else {
-      console.log("Use Infra to run the command");
-    }
-    console.log(output);
-  } else {
-    console.log("You aren't configured to grant access with Infra")
-  }
-  
-  })
-  .parse(Deno.args);
+	.name("grants")
+	.option("-u, --user <user:string>", "user email address")
+	.option("-r, --role <role:string>", "role name")
+	.option("-g, --group <group:string>", "group name")
+	.action(async ({ user = "", group = "", role = "" }) => {
+		let output = "";
+		if (await validateAccess()) {
+			if (user.length > 0 && role.length > 0) {
+				output = await addUserGrant(user, role);
+			} else if (group.length > 0 && role.length > 0) {
+				output = await addGroupGrant(group, role);
+			} else if (user.length > 0) {
+				output = await searchUser(user);
+			} else if (role.length > 0) {
+				output = await searchRole(role);
+			} else if (group.length > 0) {
+				output = await searchGroup(group);
+			} else {
+				searchAllUsers();
+			}
+
+			console.log(output);
+		} else {
+			console.log("You aren't configured to grant access with Infra");
+		}
+	})
+	.parse(Deno.args);
